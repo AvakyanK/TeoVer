067707f23f389c0f55f008717ff57dd216f6e364
AvakyanK <129741273+AvakyanK@users.noreply.github.com>
2023-04-09 23:42:17 +0300

    TeoVer

--- test/test_TeorVer.rb
+++ test/test_TeorVer.rb
@@ -3,11 +3,11 @@
 require "test_helper"
 
 class TestTeorVer < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::TeorVer::VERSION
-  end
+  #def test_that_it_has_a_version_number
+  # refute_nil ::TeorVer::VERSION
+  #end
 
-  def test_it_does_something_useful
-    assert true
-  end
+  #def test_it_does_something_useful
+  #  assert true
+  #end
 end
